a2c9b5bceab62b2d79df05b091c2457dbf696f4e
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-03-11 09:48:55 -0600

    fixing lint issues in code

--- src/ng-required-params.js
+++ src/ng-required-params.js
@@ -1,16 +1,16 @@
-(function (root, factory) {
+(function (window, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['angular'], factory);
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory(require('angular'));
   } else {
-    root.ngRequiredParams = factory(root.angular);
+    window.ngRequiredParams = factory(window.angular);
   }
-}(window, function (angular) {
+}(window, (angular) => {
   return angular.module('ng-required-params', [])
-    .factory('ngRequired', function () {
-      return param => {
+    .factory('ngRequired', () => {
+      return (param) => {
         throw new Error(`Missing required parameter: ${param}`);
       };
-    })
+    });
 }));
