88d6b7f4cf94395d54d8066169babf9b3123e1ce
schnogz <andrew@blockchain.com>
2020-01-10 13:04:38 -0500

    update deps to address security issues

--- dist/ng-required-params.js
+++ dist/ng-required-params.js
@@ -2,13 +2,13 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-(function (root, factory) {
+(function (window, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['angular'], factory);
   } else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && module.exports) {
     module.exports = factory(require('angular'));
   } else {
-    root.ngRequiredParams = factory(root.angular);
+    window.ngRequiredParams = factory(window.angular);
   }
 })(window, function (angular) {
   return angular.module('ng-required-params', []).factory('ngRequired', function () {
